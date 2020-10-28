60387d94fc293cfae9c840375a0c3475271805a7
Ewa Łobejko <lobejkoewa@gmail.com>
2020-10-28 17:46:35 +0000

    Last version

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -3,37 +3,32 @@ def arithmetic_arranger(problems, print_res = False):
   if(len(problems) > 5):
     return "Error: Too many problems."
 
-  firstNum = ""
-  secondNum = ""
-  line = ""
+  firstLine = ""
+  secondLine = ""
+  thirdLine = ""
   space = "    "
   count = 0
-  res = ""
+  resultsLine = ""
   r = 0
 
-  for elem in problems:
-    eq = elem.split()
+  for problem in problems:
+    eq = problem.split()
     count +=1
-    
     #Only  + and - operators are accepted
     if (eq[1] == "*" or eq[1] == "/"):
       return "Error: Operator must be '+' or '-'."
-    
     #Number cannot be longer than four digits
     if(len(eq[0]) > 4 or len(eq[2]) > 4):
       return "Error: Numbers cannot be more than four digits."
-
     #No spaces after last problem
     if(count == len(problems)):
       space = ""
-    
     #Number can contain inly digits
     try:
-     x = int(eq[0])
-     y = int(eq[2])
+      x = int(eq[0])
+      y = int(eq[2])
     except ValueError:
       return "Error: Numbers must only contain digits."
-
     #Calculate results
     if print_res:
       if(eq[1]== "+"):
@@ -41,23 +36,14 @@ def arithmetic_arranger(problems, print_res = False):
       else:
         r = x - y
 
-    l1 = len("  " + eq[0])
-    l2 = len(eq[1]+" "+eq[2])
+    maxlen = max(len(eq[0]), len(eq[2]))
+    firstLine = firstLine + eq[0].rjust(maxlen + 2) + space
+    secondLine = secondLine + eq[1] + eq[2].rjust(maxlen + 1) + space
+    thirdLine = thirdLine + (maxlen + 2) * "-" + space
+    resultsLine = resultsLine +  str(r).rjust(maxlen + 2) + space 
 
-    if l1 >= l2:
-      firstNum = firstNum + eq[0].rjust(l1) + space
-      secondNum = secondNum + eq[1] + eq[2].rjust(l1-1) + space
-      line = line + l1 * "-" + space
-      res = res +  str(r).rjust(l1) + space 
-    else:
-      secondNum = secondNum + eq[1]+ " " + eq[2] + space
-      firstNum = firstNum + (l2- l1 + 2) * " " + eq[0] + space
-      line = line + l2 * "-" + space
-      res = res + str(r).rjust(l2) + space
-
-  arranged_problems = firstNum + "\n" + secondNum + "\n" + line
-  
+  arranged_problems = firstLine + "\n" + secondLine + "\n" + thirdLine
   if print_res:
-    arranged_problems = arranged_problems + "\n" + res 
+    arranged_problems = arranged_problems + "\n" + resultsLine 
   
   return arranged_problems
