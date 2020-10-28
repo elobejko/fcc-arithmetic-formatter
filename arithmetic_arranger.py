61f5c0a79fdb5f9116a1c491f66a0e6759c91fd9
Ewa Łobejko <lobejkoewa@gmail.com>
2020-10-28 10:39:11 +0000

    Add second param

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,4 +1,4 @@
-def arithmetic_arranger(problems, ifprint = False):
+def arithmetic_arranger(problems, if_print_res = False):
 
   if(len(problems) > 5):
     return "Error: Too many problems."
@@ -6,28 +6,46 @@ def arithmetic_arranger(problems, ifprint = False):
   firstNum = ""
   secondNum = ""
   line = ""
+  space = "    "
+  count = 0
+  res = ""
+  r = 0
 
   for elem in problems:
     eq = elem.split()
+    count +=1
     if (eq[1] == "*" or eq[1] == "/"):
       return "Error: Operator must be '+' or '-'."
-    if(len(eq[0]) > 4 or len([2]) > 4):
+    
+    if(len(eq[0]) > 4 or len(eq[2]) > 4):
       return "Error: Numbers cannot be more than four digits."
 
-    l1 = len(eq[0])
+    if(count == len(problems)):
+      space = ""
+    
+    if if_print_res:
+      if(eq[1]== "+"):
+        r = int(eq[0] + int[eq[2]])
+      else:
+        r = int(eq[0] - int[eq[2]])
+
+    l1 = len("  " + eq[0])
     l2 = len(eq[1]+" "+eq[2])
     if l1 >= l2:
-      firstNum = firstNum + 2 * " " + eq[0] + "    "
-      secondNum = secondNum + eq[1] + (l1 - l2 + 3) * " " + eq[2] + "    "
-      line = line + l1 * "-" + "    "
+      firstNum = firstNum + 2 * " " + eq[0] + space
+      secondNum = secondNum + eq[1] + (l1 - l2 + 1) * " " + eq[2] + space
+      line = line + l1 * "-" + space
+      res = res + " " * (l1 - len(str(r))) + str(r) + space 
     else:
-      secondNum = secondNum + eq[1]+" "+eq[2] + "    "
-      firstNum = firstNum + (l2- l1 + 2) * " " + eq[0] + "    "
-      line = line + l2 * "-" + "    "
+      secondNum = secondNum + eq[1]+" "+eq[2] + space
+      firstNum = firstNum + (l2- l1 + 2) * " " + eq[0] + space
+      line = line + l2 * "-" + space
+      res = res + " " * (l1 - len(str(r)) + str(r) + space 
 
-  arranged_problems = firstNum +  "\n" + secondNum + "\n" + line
+  
+    arranged_problems = firstNum +  "\n" + secondNum + "\n" + line
+    if if_print_res:
+      arranged_problems =+res 
 
-  if ifprint:
-    print(arranged_problems)
 
   return arranged_problems
